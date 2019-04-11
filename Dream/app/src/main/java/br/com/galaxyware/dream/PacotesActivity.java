ffc7f8357b6a227390c184d1ef18e4d99c10fba9
Maiara <maiaramartins093@gmail.com>
2019-04-11 07:54:06 -0300

    first

---
+++ Dream/app/src/main/java/br/com/galaxyware/dream/PacotesActivity.java
@@ -0,0 +1,13 @@
+package br.com.galaxyware.dream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pacotes);
+    }
+}
