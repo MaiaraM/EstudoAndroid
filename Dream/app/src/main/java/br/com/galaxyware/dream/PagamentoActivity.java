bf4b39d4d02661db6e27da542e8bdfbbcd1b490c
Maiara <maiaramartins093@gmail.com>
2019-04-11 22:33:12 -0300

    pagamento

---
+++ Dream/app/src/main/java/br/com/galaxyware/dream/PagamentoActivity.java
@@ -0,0 +1,13 @@
+package br.com.galaxyware.dream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
